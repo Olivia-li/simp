c4580318904c4e25d72febbbf1b0765c3f7d61c5
Olivia Li <oliviali.ouyang@gmail.com>
2023-02-21 22:17:02 -0800

    full screen and visible over all windows

--- main.js
+++ main.js
@@ -7,10 +7,17 @@ const createWindow = () => {
       nodeIntegration: true,
       contextIsolation: false,
     },
+    minimizable: false,
+    visibleOnFullScreen: true,
+    titleBarStyle: "hidden",
+    alwaysOnTop: true,
   })
-
+  app.dock.hide()
+  win.setAlwaysOnTop(true, "screen-saver", 1)
+  win.setVisibleOnAllWorkspaces(true, {
+    visibleOnFullScreen: true,
+  })
   win.loadURL("http://localhost:3000/")
-  win.focus()
 }
 
 app.whenReady().then(() => {
