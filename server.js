2ef9a3ebf458f672e0fd11396119aca7bd29f2d0
Ben Garcia <49890553+benngarcia@users.noreply.github.com>
2023-02-21 22:37:53 -0800

    ok gonna try something

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
+  allowEIO3: true,
   cors: {
     origin: '*'
   }
