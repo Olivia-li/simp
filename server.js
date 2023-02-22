759e4a5f7e775281f0b66fe099a07998d94c3481
Olivia Li <oliviali.ouyang@gmail.com>
2023-02-22 01:04:23 -0800

    joining room user interface working

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs")
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-  res.render("landing")
+  res.render("landing", { roomId: uuidV4() })
   // res.redirect(`/${uuidV4()}`)
 })
 
