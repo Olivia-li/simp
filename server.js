606e1e41ed6f5f5985c8502d30f9e71638d05e70
Olivia Li <oliviali.ouyang@gmail.com>
2023-02-21 21:41:10 -0800

    Setup folder structure

--- server.js
+++ server.js
@@ -1,35 +1,28 @@
 const express = require("express")
 const app = express()
-
-let broadcaster
-const port = 4000
-
-const http = require("http")
-const server = http.createServer(app)
-
+const server = require("http").Server(app)
+const { v4: uuidv4 } = require("uuid")
+app.set("view engine", "ejs")
 const io = require("socket.io")(server)
-app.use(express.static(__dirname + "/public"))
+const { ExpressPeerServer } = require("peer")
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+})
+app.use("/peerjs", peerServer)
+app.use(express.static("public"))
 
-io.sockets.on("error", (e) => console.log(e))
-io.sockets.on("connection", (socket) => {
-  socket.on("broadcaster", () => {
-    broadcaster = socket.id
-    socket.broadcast.emit("broadcaster")
-  })
-  socket.on("watcher", () => {
-    socket.to(broadcaster).emit("watcher", socket.id)
-  })
-  socket.on("offer", (id, message) => {
-    socket.to(id).emit("offer", socket.id, message)
-  })
-  socket.on("answer", (id, message) => {
-    socket.to(id).emit("answer", socket.id, message)
-  })
-  socket.on("candidate", (id, message) => {
-    socket.to(id).emit("candidate", socket.id, message)
-  })
-  socket.on("disconnect", () => {
-    socket.to(broadcaster).emit("disconnectPeer", socket.id)
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`)
+})
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.param.room })
+})
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit("user-connected", userId)
   })
 })
-server.listen(port, () => console.log(`Server is running on port ${port}`))
+server.listen(3030)
