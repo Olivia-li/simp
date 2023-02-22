da80d8271e498778b5de3ab002a8a855cc71b511
Olivia Li <oliviali.ouyang@gmail.com>
2023-02-21 21:22:12 -0800

    add new server

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron")
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   })
 
-  win.loadFile('index.html')
+  win.loadFile("index.html")
 }
 
 app.whenReady().then(() => {
   createWindow()
 })
-
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+const port = 4000
+
+const http = require("http")
+const server = http.createServer(app)
+
+const io = require("socket.io")(server)
+app.use(express.static(__dirname + "/public"))
+
+io.sockets.on("error", (e) => console.log(e))
+server.listen(port, () => console.log(`Server is running on port ${port}`))
