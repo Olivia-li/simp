b5658e17119e9a29a570505f2dd428184acc0154
Olivia Li <oliviali.ouyang@gmail.com>
2023-02-21 21:52:07 -0800

    get electron to listen to express app

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
 const express = require("express")
 const app = express()
 const server = require("http").Server(app)
-const { v4: uuidv4 } = require("uuid")
-app.set("view engine", "ejs")
-const io = require("socket.io")(server)
-const { ExpressPeerServer } = require("peer")
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-})
-app.use("/peerjs", peerServer)
+
 app.use(express.static("public"))
+app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`)
+  res.render("room")
 })
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.param.room })
-})
-
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit("user-connected", userId)
-  })
-})
-server.listen(3030)
+server.listen(3000)
