52dc59312828e846c6963571c12982a1943dd3a3
Olivia Li <oliviali.ouyang@gmail.com>
2023-02-23 23:57:02 -0800

    add production server

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const createWindow = () => {
   win.setVisibleOnAllWorkspaces(true, {
     visibleOnFullScreen: true,
   })
-  win.loadURL(process.env.URL || "http://localhost:3000/")
+  win.loadURL(process.env.URL || "https://simp-production.up.railway.app/")
 
   win.on("minimize", function (event) {
     event.preventDefault()
